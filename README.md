d5900e002c9fc8a4562fcb919ede633ab7aedfde
Kazumasa Utashiro <kaz@utashiro.com>
2020-09-26 14:55:51 +0900

    minil new Text::ANSI::Printf

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Text::ANSI::Printf - It's new $module
+
+# SYNOPSIS
+
+    use Text::ANSI::Printf;
+
+# DESCRIPTION
+
+Text::ANSI::Printf is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/Text/ANSI/Printf.pm
@@ -0,0 +1,39 @@
+package Text::ANSI::Printf;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::ANSI::Printf - It's new $module
+
+=head1 SYNOPSIS
+
+    use Text::ANSI::Printf;
+
+=head1 DESCRIPTION
+
+Text::ANSI::Printf is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
